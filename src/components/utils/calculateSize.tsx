4f323e5233b113cd92261ffc5e1e1aad978d5a31
Olorunnishola Olamilekan <olorunnisholaolamilekan@gmail.com>
2023-08-17 10:58:04 +0100

    refactor: added bytes to upload file size notification

--- src/components/utils/calculateSize.tsx
+++ src/components/utils/calculateSize.tsx
@@ -1,12 +1,14 @@
-export default function (sizeInKb: number) {
-  if (sizeInKb <= 1048576) {
-    const size = sizeInKb / 1024
+export default function (sizeInBytes: number) {
+  if (sizeInBytes <= 1024) {
+    return `${sizeInBytes}b`
+  } else if (sizeInBytes > 1024 && sizeInBytes <= 1048576) {
+    const size = sizeInBytes / 1024
     return `${Math.round(size)}KB`
-  } else if (sizeInKb > 1048576 && sizeInKb <= 1073741824) {
-    const size = sizeInKb / 1048576
+  } else if (sizeInBytes > 1048576 && sizeInBytes <= 1073741824) {
+    const size = sizeInBytes / 1048576
     return `${size.toPrecision(2)}MB`
   } else {
-    const size = sizeInKb / 1099511627776
+    const size = sizeInBytes / 1099511627776
     return `${size.toPrecision(2)}GB`
   }
 }
